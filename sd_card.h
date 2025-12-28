7e24047415a10684af9e4f2caeef125f8427f1d0
Daniil <ay.thug2005@gmail.com>
2025-12-28 18:28:17 +0700

    Update sd_card.h

--- sd_card.h
+++ sd_card.h
@@ -17,15 +17,16 @@ typedef enum {
 
 /* Контекст SD-карты */
 typedef struct {
-    uint8_t initialized;                    // карта инициализирована?
-    uint8_t response[SD_RESPONSE_SIZE];     // ответы SD
-    SD_Status_t error;                      // код ошибки
-    uint8_t *data;                          // указатель на буфер данных
+    uint8_t initialized;
+    uint8_t response[SD_RESPONSE_SIZE];
+    SD_Status_t error;
+    uint8_t *data;
 } SD_Handle_t;
 
 /* API */
 SD_Status_t SD_Init(SD_Handle_t *sd);
 SD_Status_t SD_SendCmd(SD_Handle_t *sd, uint8_t cmd, uint32_t arg);
 SD_Status_t SD_ReadBlock(SD_Handle_t *sd, uint32_t block);
+SD_Status_t SD_WriteBlock(SD_Handle_t *sd, uint32_t block);
 
 #endif /* SD_CARD_H */
